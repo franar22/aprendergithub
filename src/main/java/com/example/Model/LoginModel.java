2dee11aefa523930b77dbfeb20b941eea9947650
Franar22 <fran.argandona@duocuc.cl>
2025-05-06 18:11:03 -0400

    ahor si agreugue clase

--- src/main/java/com/example/Model/LoginModel.java
+++ src/main/java/com/example/Model/LoginModel.java
@@ -7,3 +7,4 @@ public class LoginModel {
     private String correo;
     private String contraseña;
 }
+f4gu34tpg4tughgutgun5ngtgnwedijepfourgpb24tgféiof3rpgu4tpgrngptugrn4p
---
+++ src/test/java/com/example/login/probando/rfuith45.java
@@ -0,0 +1,10 @@
+package com.example.login.probando;
+
+import org.junit.jupiter.api.Test;
+
+public class rfuith45 {
+    @Test
+	void contextLoads() {
+	}
+
+}
